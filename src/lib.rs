dbb403e0d1bddd0fce842589b830ea219a58766f
John T. Wodder II <git@varonathe.org>
2023-10-28 16:49:50 -0400

    To & from strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use cfg_if::cfg_if;
 
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(
+    Clone, Copy, Debug, strum::Display, strum::EnumString, Eq, Hash, Ord, PartialEq, PartialOrd,
+)]
+#[strum(ascii_case_insensitive, serialize_all = "lowercase")]
 pub enum Format {
     Json,
     Toml,
@@ -34,6 +37,63 @@ impl Format {
         match self {
             Format::Json => &["json"],
             Format::Toml => &["toml"],
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod json {
+        use super::*;
+
+        #[test]
+        fn basics() {
+            let f = Format::Json;
+            assert_eq!(f.to_string(), "json");
+            assert_eq!(f.extensions(), ["json"]);
+            assert_eq!("json".parse::<Format>().unwrap(), f);
+            assert_eq!("JSON".parse::<Format>().unwrap(), f);
+            assert_eq!("Json".parse::<Format>().unwrap(), f);
+        }
+
+        #[cfg(feature = "json")]
+        #[test]
+        fn enabled() {
+            assert!(Format::Json.is_enabled());
+        }
+
+        #[cfg(not(feature = "json"))]
+        #[test]
+        fn not_enabled() {
+            assert!(!Format::Json.is_enabled());
+        }
+    }
+
+    mod toml {
+        use super::*;
+
+        #[test]
+        fn basics() {
+            let f = Format::Toml;
+            assert_eq!(f.to_string(), "toml");
+            assert_eq!(f.extensions(), ["toml"]);
+            assert_eq!("toml".parse::<Format>().unwrap(), f);
+            assert_eq!("TOML".parse::<Format>().unwrap(), f);
+            assert_eq!("Toml".parse::<Format>().unwrap(), f);
+        }
+
+        #[cfg(feature = "toml")]
+        #[test]
+        fn enabled() {
+            assert!(Format::Toml.is_enabled());
+        }
+
+        #[cfg(not(feature = "toml"))]
+        #[test]
+        fn not_enabled() {
+            assert!(!Format::Toml.is_enabled());
         }
     }
 }
